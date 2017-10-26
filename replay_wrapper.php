f16b78651328ab9178942c52a20a43ed416fbf25
TomMuc1 <31601862+TomMuc1@users.noreply.github.com>
2017-10-27 00:32:17 +0200

    Update replay_wrapper.php

--- replay_wrapper.php
+++ replay_wrapper.php
@@ -3,8 +3,10 @@
 // set hex and date you want to replay and set $i to timelapse-factor e.g. 10 is 10-times faster than reality
 $user_hex = 'YOUR_HEX';    $date_to_watch = 'YYYY-MM-DD%';    $speed_factor = 10;
 
+$sql = "select * from aircrafts where hex='$user_hex' and message_date like '$date_to_watch' and squawk!='' and flight!='' and lat!='' and lon!='' and nucp!='' and seen_pos!='' and altitude!='' and vert_rate!='' and track!='' and speed!='' and category!='' and messages!='' and seen!='' and rssi!='' order by id asc";
 $db = new PDO('mysql:host=127.0.0.1;dbname=adsb', 'root', 'YOUR_PASSWORD');
-$stmt = $db->prepare("select * from aircrafts where hex='" . $user_hex . "' and message_date like '" . $date_to_watch . "' order by id asc");
+$stmt = $db->prepare($sql);
+
 $stmt->execute();
 $select = $stmt->fetchAll();
 
@@ -15,7 +17,7 @@ foreach  ($select as $result) {
 $aircraft_json = '{ "now" : ' . $result['now'] . ',' . PHP_EOL;
 $aircraft_json .= '  "messages" : 60296972,' . PHP_EOL;
 $aircraft_json .= '  "aircraft" : [' . PHP_EOL;
-$aircraft_json .= '    {"hex":"' . $result['hex'] . '","squawk":"' . $result['squawk'] . '","flight":"' . $result['flight'] . '","lat":' . $result['lat'] . ',"lon":' . $result['lon'] . ',"nucp":' . $result['nucp'] . ',"seen_pos":' . $result['seen_pos'] . ',"altitude":' . $result['altitude'] . ',"vert_rate":' . $result['vert_rate'] . ',"track":' . $result['track'] . ',"speed":' . $result['speed'] . ',"category":"' . $result['category'] . '","mlat":[],"tisb":[],"messages":' . $result['messages'] . ',"seen":' . $result['seen'] . ',"rssi":' . $result['rssi'] . '}' . PHP_EOL;
+$aircraft_json .= '    {"hex":"' . $result['hex'] . '","squawk":"' . $result['squawk'] . '","flight":"' . $result['flight'] . '","lat":' . $result['lat'] . ',"lon":' . $result['lon'] . ',"nucp":' . $result['nucp'] . ',"seen_pos":' . $result['seen_pos'] . ',"altitude":' . $result['altitude'] . ',"vert_rate":' . $result['vert_rate'] . ',"track":' . $result['track'] . ',"speed":' . $result['speed'] . ',"category":"' . $result['category'] . '","mlat":"","tisb":"","messages":' . $result['messages'] . ',"seen":' . $result['seen'] . ',"rssi":' . $result['rssi'] . '}' . PHP_EOL;
 $aircraft_json .= '  ]' . PHP_EOL;
 $aircraft_json .= '}' . PHP_EOL;
 $i++;
