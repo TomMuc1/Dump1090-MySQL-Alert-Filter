e982bea9d903a9d2cb5707e956223ae6e10a3583
TomMuc1 <31601862+TomMuc1@users.noreply.github.com>
2017-10-26 21:38:20 +0200

    Create replay_wrapper.php

---
+++ replay_wrapper.php
@@ -0,0 +1,27 @@
+<?php
+
+// set hex and date you want to replay and set $i to timelapse-factor e.g. 10 is 10-times faster than reality
+$user_hex = 'YOUR_HEX';    $date_to_watch = 'YYYY-MM-DD%';    $speed_factor = 10;
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=adsb', 'root', 'YOUR_PASSWORD');
+$stmt = $db->prepare("select * from aircrafts where hex='" . $user_hex . "' and message_date like '" . $date_to_watch . "' order by id asc");
+$stmt->execute();
+$select = $stmt->fetchAll();
+
+$i = 0;
+
+foreach  ($select as $result) {
+
+$aircraft_json = '{ "now" : ' . $result['now'] . ',' . PHP_EOL;
+$aircraft_json .= '  "messages" : 60296972,' . PHP_EOL;
+$aircraft_json .= '  "aircraft" : [' . PHP_EOL;
+$aircraft_json .= '    {"hex":"' . $result['hex'] . '","squawk":"' . $result['squawk'] . '","flight":"' . $result['flight'] . '","lat":' . $result['lat'] . ',"lon":' . $result['lon'] . ',"nucp":' . $result['nucp'] . ',"seen_pos":' . $result['seen_pos'] . ',"altitude":' . $result['altitude'] . ',"vert_rate":' . $result['vert_rate'] . ',"track":' . $result['track'] . ',"speed":' . $result['speed'] . ',"category":"' . $result['category'] . '","mlat":[],"tisb":[],"messages":' . $result['messages'] . ',"seen":' . $result['seen'] . ',"rssi":' . $result['rssi'] . '}' . PHP_EOL;
+$aircraft_json .= '  ]' . PHP_EOL;
+$aircraft_json .= '}' . PHP_EOL;
+$i++;
+
+if ($i == $speed_factor) { file_put_contents('/YOUR_PATH/data/aircraft.json', $aircraft_json, LOCK_EX); $i = 0; sleep(1); }
+
+}
+
+?>
